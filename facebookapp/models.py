ced15b49bc7d2c63eb07228a3f623b25655b378c
akhil118 <gandikota.abai@gmail.com>
2022-07-21 22:16:00 +0530

    updated code

---
+++ facebookapp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+
+# Create your models here.
+
+
+class Profilemodel(models.Model):
+    image = models.ImageField(upload_to="ProfilePics")
+    bio = models.CharField(max_length=200)
+    nickname=models.CharField(max_length=100)
+    followers = models.ManyToManyField(User,related_name='followers',blank=True,null=True)
+    following  = models.ManyToManyField(User,blank=True,null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="user")
+
+class Post(models.Model):
+    postImg = models.ImageField(upload_to="posts",blank=True,null=True)
+    text = models.CharField(max_length=200)
+    profileuser = models.ForeignKey(Profilemodel,related_name="profile",on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User,related_name="likes",blank=True,null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    time = models.DateTimeField(auto_now_add=True, blank=True)
+
+class comment(models.Model):
+   
+    comment1 = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    parent =models.ForeignKey('self',on_delete=models.CASCADE, null=True)
+    time = models.DateTimeField(auto_now_add=True, blank=True)
+    
+    
+    
---
+++ facebookapp/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from facebookapp import views
+
+
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('login',views.login1,name='login'),
+    path('logout',views.logout,name='logout'),
+    path('comments/logout',views.logout,name='logout'),
+    path('register',views.register,name='register'),
+    path('profile',views.profile,name='profile'),
+    path('updateProfile',views.updateProfile,name='updateProfile'),
+    path('friends',views.friends,name='friends'),
+    path('updatePost',views.updatePost,name='updatePost'),
+    path('likePost/<int:id>',views.likePost,name='likePost'),
+    path('search',views.search,name='search'),
+    path('follow',views.follow,name='follow'),
+    path('comments/<int:id>',views.comments,name='comments'),
+    path('comments/postComment',views.postComment,name='postComment'),
+]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+
+# handler404 = 'facebookapp.views.notfound'
+
